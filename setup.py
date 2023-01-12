0d5911a1f2f835b0db13c570845c044e2ae44fb6
daveturn <david@turning.ai>
2023-01-12 11:17:28 -0600

    added find_packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if sys.version_info[0] < 3:
     with open("README.md", "r") as fh:
@@ -12,13 +12,13 @@ else:
 
 setup(
     name="turn_alerts",
-    version="1.0.0",
+    version="1.0.1",
     description="Client library for use with the Turn notifications microservice",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Amir Canto",
     author_email="amir@turn.ai",
-    packages=["turn_alerts", "turn_alerts.schemas"],
+    packages=find_packages(),
     install_requires=["marshmallow>=3.19.0", "requests>=2.28.1"],
     tests_require=["nose>=1.3.7"],
     test_suite="nose.collector",
